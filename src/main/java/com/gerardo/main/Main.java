b0d97ad48ece2cf963317d9293edf0e25e43b6c5
jerry_04 <gerardo.garcia.q@gmail.com>
2022-02-22 15:40:27 -0600

    add class main and utils

---
+++ src/main/java/com/gerardo/main/Main.java
@@ -0,0 +1,11 @@
+package com.gerardo.main;
+
+import static com.gerardo.utils.Utils.obtenerCadena;
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(obtenerCadena("vamos a estar  bien"));
+    }
+
+}
---
+++ src/main/java/com/gerardo/utils/Utils.java
@@ -0,0 +1,9 @@
+package com.gerardo.utils;
+
+public class Utils {
+
+  public static  String obtenerCadena(String value) {
+   return value.substring(0,8);
+  }
+
+}
